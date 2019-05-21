120cc45d947a1898924aa5e17502eaf088662063
Italo Lelis <italohdc@gmail.com>
2019-05-21 19:42:21 -0300

    Create base template

---
+++ src/components/BusCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card
+    class="ma-3"
+    color="blue-grey lighten-5"
+    dark
+    ripple
+    hover
+  >
+    <v-card-title class="blue-grey lighten-1">
+      <div>
+        <h3 class="headline">{{ line }}</h3>
+        <span class="subheading">Período Letivo</span>
+      </div>
+      <v-spacer></v-spacer>
+      <div>
+        <span class="subheading">em aproximadamente</span>
+        <h3 class="title">{{ eta }} minutos</h3>
+      </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "BusCard",
+  props: {
+    line: String,
+    eta: Number
+  }
+}
+</script>
---
+++ src/components/NextBus.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{startPoint}}</p>
+    <p>{{endPoint}}</p>
+    <BusCard
+      line="Linha 1"
+      :eta="10"
+    ></BusCard>
+  </div>
+</template>
+
+<script>
+import BusCard from './BusCard';
+
+export default {
+  name: 'NextBus',
+  components: {
+    BusCard
+  },
+  props: {
+    startPoint: { type: [Object], required: false },
+    endPoint: { type: [Object], required: false }
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
