a61b47d7241ec8ad9939d398bf459afb7fdef026
Italo Lelis <italohdc@gmail.com>
2019-05-24 00:06:38 -0300

    Add header and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-footer height="auto" light>
+    <v-layout justify-center row wrap light>
+      <v-card-text>
+        Este é um projeto de código aberto desenvolvido utilizando Vue.js
+      </v-card-text>
+      <v-btn v-for="link in links" :key="link.text" :href="link.url" target="_blank" flat round>
+        {{ link.text }}
+      </v-btn>
+      <v-flex grey lighten-2 py-3 text-xs-center xs12>
+        Made with <v-icon small>favorite</v-icon> by
+        <a class="black--text" href="https://italolelis.com" target="_blank">
+          <strong>Italo Lelis</strong>
+        </a>
+      </v-flex>
+    </v-layout>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data: () => ({
+    links: [
+      {
+        text: 'Sobre',
+        url: 'https://github.com/italohdc/UFMG-Bus'
+      },
+      {
+        text: 'GitHub',
+        url: 'https://github.com/italohdc/UFMG-Bus'
+      },
+      {
+        text: 'Licença',
+        url: 'https://github.com/italohdc/UFMG-Bus/blob/master/LICENSE'
+      }
+    ]
+  })
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div dark class="light-blue darken-4 pa-4" justify-center>
+    <div class="app-center-container">
+      <img id="header-img" src="../assets/logo.png" alt="Ônibus UFMG">
+      <v-card-text class="white--text subheading">
+        Horários dos próximos ônibus internos do campus Pampulha
+      </v-card-text>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+#header-img {
+  max-width: 100%;
+}
+</style>
